0adacac004b8abd8e46c40406eaa6b57fb8d2a62
Daniel Vogel <daniel.vogel@hallowelt.com>
2024-11-12 10:52:00 +0100

    Fix pdf export of diff view
    [4.4.x]
    
    ERM39221
    
    Change-Id: I6377ebeffddd0417456cba9fd1ac487fbe2c3b99
    (cherry picked from commit 4078fe8aa69471dfc97984eba4b6abd614feb1f8)

--- src/Hook/BSUEModulePDFBeforeCreatePDF/HideUnselectedDiffs.php
+++ src/Hook/BSUEModulePDFBeforeCreatePDF/HideUnselectedDiffs.php
@@ -10,10 +10,15 @@ class HideUnselectedDiffs extends BSUEModulePDFBeforeCreatePDF {
 	 * @return bool
 	 */
 	protected function doProcess() {
+
+		if ( $this->specs->getParam( 'difftab' ) === null ) {
+			return true;
+		}
+
 		$finder = new DOMXPath( $this->DOM );
 		$containerElements = $finder->query( "//*[contains(@class, 'diffcontainer')]" );
 		foreach ( $containerElements as $containerElement ) {
-			if ( $containerElement->getAttribute( 'id' ) == $this->caller->aParams['difftab'] ) {
+			if ( $containerElement->getAttribute( 'id' ) == $this->specs->getParam( 'difftab' ) ) {
 				// Keep user selected
 				continue;
 			}
